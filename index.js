2fd559a9ec175cb3121f030088318c692eef926f
Jonas <mz-martinez@live.com>
2022-05-08 17:32:50 -0300

    Patch user

--- index.js
+++ index.js
@@ -54,4 +54,20 @@ app.post('/users', async (req, res) =>{
 
 })
 
+app.patch('/users/:id', async (req, res) =>{
+
+    try {
+
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndUpdate(id, req.body, {new:true})
+
+        res.status(200).json(user);
+        
+    } catch (error) {
+
+        res.status(500).send(error.message);
+        
+    }
+})
+
 app.listen(3000, console.log("Server is running (3000)..."));
